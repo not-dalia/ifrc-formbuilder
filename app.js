7cdcde313f3de88f88a3244678df5f36ce80f75d
Local Admin <LocalAdmin@16DCompL069.campus.ncl.ac.uk>
2018-04-25 11:05:35 +0100

    fixed bug

--- app.js
+++ app.js
@@ -43,18 +43,9 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-app.post('/generate_workshop', function (req, res, next) {
+app.post('/generate_workshop/', function (req, res, next) {
   try {
     if (!req.body.title || !req.body.language || req.body.tasks) throw new Error('Missing fields')
     let path = req.body.name.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "") + Date.now();
@@ -90,6 +81,18 @@ app.get('/workshops/:workshopPath', function (req, res, next) {
   } catch (err) {
     next(createError(404));
   }
+});
+
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
